9b894ce1e8dfd8279db814936fe45f14a1ed8ad2
yashguy65 <yashguy65@gmail.com>
2026-04-26 00:50:03 +0530

    feat: added springboot

---
+++ apps/backend/skillfuze/src/main/java/com/skillfuze/skillfuze/SkillfuzeApplication.java
@@ -0,0 +1,13 @@
+package com.skillfuze.skillfuze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillfuzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillfuzeApplication.class, args);
+	}
+
+}
